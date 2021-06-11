4c44df601dd68d23e39b83515ec9008e4af8f607
Mohammad Tanvir Islam <tanvieer@gmail.com>
2021-06-11 13:17:30 +0600

    sample values api

--- Application/SchoolMgmt/SchoolMgmt/Controllers/ValuesController.cs
+++ Application/SchoolMgmt/SchoolMgmt/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SchoolMgmt.Model.ViewModel;
 using System;
 using System.Collections.Generic;
@@ -44,5 +45,53 @@ namespace SchoolMgmt.Controllers
         public void Delete(int id)
         {
         }
+
+
+
+
+
+
+
+        //[Authorize(Roles = "Administrator")]
+        [Authorize(Roles = "Manager")]
+        [HttpGet("manager")]
+        public ActionResult GetManager()
+        {
+            return Ok("Sucess!");
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpGet("admin")]
+        public ActionResult GetAdmin()
+        {
+            return Ok("Sucess!");
+        }
+
+        [Authorize(Roles = "User")]
+        [HttpGet("user")]
+        public ActionResult GetUser()
+        {
+            return Ok("Sucess!");
+        }
+
+
+
+        [Authorize(Roles = "Administrator")]
+        [Authorize(Roles = "User")]
+        [Authorize(Roles = "Manager")]
+        [HttpGet("all")]
+        public ActionResult GetAll()
+        {
+            return Ok("Sucess!");
+        }
+
+         
+
+        [HttpGet("me")]
+        public ActionResult Gett()
+        {
+            return Ok("Sucess!");
+        }
+
     }
 }
