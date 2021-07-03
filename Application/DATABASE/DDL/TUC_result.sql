b58fe9e640360f8b7e28dafc35dd9dc8b6361264
Mohammad Tanvir Islam <tanvieer@gmail.com>
2021-07-04 02:08:10 +0600

    FOLDER STRUCTURE CHANGED

---
+++ Application/DATABASE/DDL/TUC_result.sql
@@ -0,0 +1,38 @@
+-- Create table
+create table TUC_RESULT
+(
+  result_id        NUMBER(5) not null,
+  test_id          NUMBER(5) not null,
+  grade            NUMBER(8,2) not null,
+  status           CHAR(1) default 'R' not null,
+  maker_id         VARCHAR2(100) default 'SYSTEM' not null,
+  maker_time       DATE default SYSDATE not null,
+  last_update_by   VARCHAR2(100) default 'SYSTEM' not null,
+  last_update_time DATE default SYSDATE not null
+)
+tablespace USERS
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+  );
+-- Create/Recreate primary, unique and foreign key constraints 
+alter table TUC_RESULT
+  add constraint TUC_RESULT_PK primary key (RESULT_ID)
+  using index 
+  tablespace USERS
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+  );
